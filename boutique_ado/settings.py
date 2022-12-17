220d6e22cff3a486b5113ae1df51a40a5f47b600
Nigel Doheny <nigel.doheny93@gmail.com>
2022-12-17 11:19:37 +0000

    Added sorting JS and back to top link and delivery logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
                 # `allauth` needs this from django
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -160,6 +161,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
