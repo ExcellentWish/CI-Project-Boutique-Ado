678cd67fc2ab1b1ac9be4abcb17c2ac2590b2f59
Nigel Doheny <nigel.doheny93@gmail.com>
2022-12-18 15:19:51 +0000

    Added add to bag functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -2,7 +2,6 @@ from decimal import Decimal
 from django.conf import settings
 
 def bag_contents(request):
-
     bag_items = []
     total = 0
     product_count = 0
--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -107,7 +107,7 @@ LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/'
 
 
-CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1:8000','https://8000-excellentwi-ciprojectbo-291qhpdlijz.ws-eu78.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1:8000','https://8000-excellentwi-ciprojectbo-291qhpdlijz.ws-eu79.gitpod.io']
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
